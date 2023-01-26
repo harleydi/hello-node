d42535972cd93b0d136d827f0f221974131c97c3
harleydi <disneyharley26@gmail.com>
2023-01-26 13:06:16 -0500

    completed stretch goals

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ console.log('HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd
 console.log('HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!')
 console.log('HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd')
 console.log('HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!')
+
+
+console.log(' _   _                  _                ')
+console.log('| | | |   __ _   _ __  | |   ___   _   _ ')
+console.log(`| |_| |  / _  | | '__| | |  / _ \ | | | |`)
+console.log('|  _  | | (_| | | |    | | |  __/ | |_| |')
+console.log('|_| |_|  \__,_| |_|    |_|  \___|  \__, |')
+console.log('                                   |___/ ')
